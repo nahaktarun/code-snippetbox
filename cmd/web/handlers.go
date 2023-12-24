6b3e1a7890c2c1be33cec292a8793ef0de90587f
Tarun Nahak <tarunnahak25@gmail.com>
2023-12-24 11:55:40 +0530

    Added templates and partial templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -17,14 +17,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 
 	}
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal server Error", http.StatusInternalServerError)
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 
 	if err != nil {
 		log.Print(err.Error())
