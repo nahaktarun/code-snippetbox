1f79a12b13b050ee3f75c55dd2b2c0619ba74687
Tarun Nahak <tarunnahak25@gmail.com>
2023-12-27 14:43:39 +0530

    Added routing

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,37 +6,31 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/julienschmidt/httprouter"
 	"snippetbox.tarunnahak.in/internal/models"
 )
 
 // Define a home handler function which writes a byte slice containing response body
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-
-	}
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
-
 	data := app.newTemplateData(r)
 	data.Snippets = snippets
-
 	app.render(w, r, http.StatusOK, "home.tmpl.html", data)
 }
 
 // Add a snippetView handler function
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	params := httprouter.ParamsFromContext(r.Context())
 
-	if err != nil || id < 0 {
+	id, err := strconv.Atoi(params.ByName("id"))
+
+	if err != nil || id < 1 {
 		app.notFound(w)
 		return
 	}
@@ -58,15 +52,13 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// Add a snippetCreate handler function
+// Add a snippetCreate for the form template
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display the form for creating a new snippet"))
+}
 
-	if r.Method != http.MethodPost {
-		w.Header().Set("allow", http.MethodPost)
-
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+// Add a snippetCreatePost handler function
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
 	title := "O snail"
 	content := "o snail test"
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,25 +3,26 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 // the routes() method returns a servermux containing our application routes
 
 func (app *application) routes() http.Handler {
-
-	mux := http.NewServeMux()
-
+	// Initialize the router.
+	router := httprouter.New()
+	// Update the pattern for the route for the static files.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	// And then create the routes using the appropriate methods, patterns and
+	// handlers.
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
+	// Create the middleware chain as normal.
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-
-	return standard.Then(mux)
-
+	// Wrap the router with the middleware and return it as normal.
+	return standard.Then(router)
 }
