b2ae87b024a537d56c4367fcb9569cfd7a821015
Tarun Nahak <tarunnahak25@gmail.com>
2023-12-27 22:54:18 +0530

    parsing form data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -61,16 +61,24 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 // Add a snippetCreatePost handler function
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
-	title := "O snail"
-	content := "o snail test"
-	expires := 7
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 	id, err := app.snippets.Insert(title, content, expires)
-
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
-
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
